61ebebe61655a6fe439912ca2380babfc550f78f
Lucas <lucaslustosadias@gmail.com>
2025-03-31 16:13:37 -0300

    Fiz a classe Team, com suas relacoes com User e Task

---
+++ src/main/java/com/example/taskflow/entities/Team.java
@@ -0,0 +1,70 @@
+package com.example.taskflow.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Entity
+@Table(name="tb_teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    @ManyToMany()
+    private List<User> members;
+    @OneToMany()
+    private List<Task> tasks;
+    public Team() {}
+
+    public Team(Long id, String name, String description, LocalDateTime createdAt, List<User> members, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.members = members;
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public List<User> getMembers() {
+        return members;
+    }
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+
+
+}
